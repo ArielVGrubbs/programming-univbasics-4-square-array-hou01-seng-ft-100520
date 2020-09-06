2963c88dfca636f4eb137ccc43b9cac212e310cd
Ariel Grubbs <arielvictor1234@gmail.com>
2020-09-06 23:22:51 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,6 +2,8 @@ def square_array(array)
   counter = 0
   new_array = []
   while counter < array.length {
-    new_array.push(array[index] ** )
+    new_array.push(array[index] ** 2)
+    counter += 1
+    return new_array
   }
 end
